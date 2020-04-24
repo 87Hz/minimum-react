a8d80b63b88e33ef0a1cfc3adf4fea004b9e1238
HU Ze <huze@outlook.sg>
2020-04-24 20:16:43 +1200

    render AppWithReduxStore

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import { App } from '~/App'
+import { AppWithReduxStore } from '~/AppWithReduxStore'
 
-render(<App />, document.getElementById('app'))
+render(<AppWithReduxStore />, document.getElementById('app'))
