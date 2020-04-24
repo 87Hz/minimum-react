b3a9fdb98c209ba515298d436f9f0529d1cd02e4
HU Ze <huze@outlook.sg>
2020-04-24 20:51:26 +1200

    try props as cssVar in styled API

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,12 @@
 import { styled } from 'linaria/react'
+import { prop } from 'ramda'
 
 export const Button = styled.button`
+  --border-color: ${prop<any, any>('borderColor')};
+
   color: var(--text-color);
   background-color: var(--color-primary);
-  font-size: ${(props: any) => props.fontSize};
+
+  border-color: var(--border-color);
+  font-size: ${prop<any, any>('fontSize')};
 `
--- src/components/DynamicButtonDemo.tsx
+++ src/components/DynamicButtonDemo.tsx
@@ -5,5 +5,11 @@ import { ThemeContext, Themes } from '~/components/ThemeProvider'
 export const DynamicButtonDemo = () => {
   const { theme } = useContext(ThemeContext)
   const fontSize = theme === Themes.Dark ? 50 : 20
-  return <Button fontSize={fontSize}>FontSize Varies with theme</Button>
+  const borderColor = theme === Themes.Dark ? 'green' : 'red'
+
+  return (
+    <Button fontSize={fontSize} borderColor={borderColor}>
+      FontSize Varies with theme
+    </Button>
+  )
 }
