0353e02356ade4be0b2f399323ed14a6c4fb0bcc
HU Ze <huze@outlook.sg>
2020-04-24 20:08:25 +1200

    added RestyledButton

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Button } from '~/components/Button'
 import { ThemeProvider } from '~/components/ThemeProvider'
 import { ThemeToggle } from '~/components/ThemeToggle'
 import { ThemeReader } from '~/components/ThemeReader'
+import { RestyledButton } from '~/components/RestyledButton'
 
 export const App = () => {
   return (
     <ThemeProvider>
       <p>Hello</p>
       <Button>Btn</Button>
+      <RestyledButton>RestyledBtn</RestyledButton>
       <ThemeToggle />
       <ThemeReader />
     </ThemeProvider>
---
+++ src/components/RestyledButton.tsx
@@ -0,0 +1,6 @@
+import { styled } from 'linaria/react'
+import { Button } from './Button'
+
+export const RestyledButton = styled(Button)`
+  font-size: 50px;
+`
